2bd739e60837a762aa9389d9c434a1ecf051237c
kamelkholy <kamelabdelkader17@gmail.com>
2021-01-25 16:48:33 +0200

    add order model and migration

---
+++ database/migrations/2021_01_25_143812_create_orders_table.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('phone');
+            $table->string('email');
+            $table->text('address');
+            $table->string('city');
+            $table->string('province');
+            $table->enum('payment_method', ['cash', 'visa']);
+            $table->longText('products');
+            $table->decimal('total');
+            $table->decimal('sub_total');
+            $table->decimal('shipping_fees');
+            $table->integer('quantity');
+            $table->string('status');
+            $table->text('customer_message');
+            $table->text('staff_notes');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+}
