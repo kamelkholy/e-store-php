89956086bb29acd74404982e5886e131ba169bac
kamelkholy <kamelabdelkader17@gmail.com>
2021-02-08 13:29:09 +0200

    - place order - show sliders - show featured categories

--- app/Http/Controllers/StoreController.php
+++ app/Http/Controllers/StoreController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Response;
+use Illuminate\Support\Facades\Validator;
 use App\Models\Product;
 use App\Models\Brand;
 use App\Models\Category;
@@ -11,6 +12,10 @@ use App\Models\CategoriesLinker;
 use App\Models\ProductImages;
 use App\Models\ProductTags;
 use App\Models\CityShipping;
+use App\Models\FeaturedCategory;
+use App\Models\Order;
+use App\Models\OrderStatus;
+use App\Models\Slider;
 use App\Models\StoreSettings;
 
 use Image;
@@ -28,8 +33,37 @@ class StoreController extends Controller
         }
         $children = new CategoriesLinker();
         $children = $children->getChildren(($parents));
-
-        return view('store', ['brands' => $brands, 'parents' => $parentsData, 'children' => $children]);
+        $sliders = Slider::all()->sortBy('sortOrder');
+        $featuredCategories = FeaturedCategory::leftJoin('categories as c', 'category', '=', 'c.id')
+            ->leftJoin('products as p', 'featured_product', '=', 'p.id')
+            ->leftJoin('product_images as pi', function ($q) {
+                $q->on('pi.product', '=', 'featured_categories.featured_product')
+                    ->on(
+                        'pi.id',
+                        '=',
+                        DB::raw('(select min(id) from product_images where product = pi.product)')
+                    );
+            })->select('p.id as featured_product_id', 'p.name as featured_product_name', 'p.price as featured_product_price', 'p.enable_discount as featured_product_enable_discount', 'p.discount as featured_product_discount', 'pi.image as featured_product_image', 'featured_categories.*', 'c.name as category_name')->get()->sortBy('sortOrder');
+        foreach ($featuredCategories as $key => $value) {
+            $value->final_price = $value->featured_product_price - ($value->featured_product_price * $value->featured_product_discount) / 100;
+            $products = Product::leftJoin('product_images as pi', function ($q) {
+                $q->on('pi.product', '=', 'products.id')
+                    ->on(
+                        'pi.id',
+                        '=',
+                        DB::raw('(select min(id) from product_images where product = pi.product)')
+                    );
+            })
+                ->select('products.*', 'pi.image as image')
+                ->where('category', $value->category)->limit($value->products_limit)->get()->sortBy('sortOrder');
+            for ($i = 0; $i < count($products); $i++) {
+                if ($products[$i]->enable_discount) {
+                    $products[$i]->final_price = $products[$i]->price - ($products[$i]->price * $products[$i]->discount) / 100;
+                }
+            }
+            $value->products = $products;
+        }
+        return view('store', ['featuredCategories' => $featuredCategories, 'sliders' => $sliders, 'brands' => $brands, 'parents' => $parentsData, 'children' => $children]);
     }
     function cart()
     {
@@ -46,6 +80,7 @@ class StoreController extends Controller
     }
     function checkout(Request $request)
     {
+        $request->validate(['products' => 'required']);
         $productIds = [];
         $shippingFees = 0;
         $total = 0;
@@ -53,18 +88,9 @@ class StoreController extends Controller
         foreach ($request->products as $key => $value) {
             array_push($productIds, $value['id']);
         }
-        $settings = StoreSettings::where([])->get();
-        if (count($settings) == 0) {
-            $form_data = array(
-                'address'  => '',
-                'email'  => '',
-                'phone'  => '',
-                'flat_shipping'  => 50,
-            );
-            $settings = StoreSettings::create($form_data);
-        } else {
-            $settings = $settings[0];
-        }
+        $settings = new StoreSettings();
+        $settings = $settings->findOrCreate();
+
         $products = Product::whereIn('id', $productIds)->select('id', 'name', 'price', 'quantity', 'shippingType', 'shipping_fees', 'enable_discount', 'discount')->get();
         for ($i = 0; $i < count($products); $i++) {
             $products[$i]->bought_qty = $request->products[$products[$i]->id]['quantity'];
@@ -97,6 +123,118 @@ class StoreController extends Controller
             'total' => $total,
             'isCustomShipping' => $isCustomShipping
         ]);
+    }
+    function placeOrder(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'products' => 'required',
+            'first_name' => 'required',
+            'last_name' => 'required',
+            'address' => 'required',
+            'city' => 'required',
+            'phone' => 'required',
+            'email' => 'required',
+            'payment' => 'required',
+        ]);
+
+        if ($validator->fails()) {
+            return redirect()
+                ->route('store.cart')
+                ->withErrors($validator)
+                ->withInput();
+        }
+
+        $productIds = [];
+        $orderProducts = [];
+        $shippingFees = 0;
+        $total = 0;
+        $total_qty = 0;
+        $isCustomShipping = false;
+        foreach ($request->products as $key => $value) {
+            array_push($productIds, $value['id']);
+        }
+        $settings = new StoreSettings();
+        $settings = $settings->findOrCreate();
+
+        $products = Product::whereIn('id', $productIds)->select('id', 'name', 'price', 'quantity', 'shippingType', 'shipping_fees', 'enable_discount', 'discount')->get();
+        for ($i = 0; $i < count($products); $i++) {
+            $products[$i]->bought_qty = $request->products[$products[$i]->id]['quantity'];
+            if ($products[$i]->bought_qty > $products[$i]->quantity) {
+                return redirect()->route('store.cart')->withErrors(['message' => "Not Enough in Stock"]);
+            }
+            if ($products[$i]->enable_discount) {
+                $products[$i]->final_price = $products[$i]->price - ($products[$i]->price * $products[$i]->discount) / 100;
+                $total += $products[$i]->final_price * $products[$i]->bought_qty;
+            } else {
+                $total += $products[$i]->price * $products[$i]->bought_qty;
+            }
+            $productShipping = 0;
+            switch ($products[$i]->shippingType) {
+                case 'calculated':
+                    $productShipping = $products[$i]->shipping_fees * $products[$i]->bought_qty;
+                    $isCustomShipping = true;
+                    break;
+                case 'flat':
+                    $productShipping = $settings->flat_shipping;
+                    break;
+                case 'free':
+                    break;
+            }
+            $total_qty += $products[$i]->bought_qty;
+            $shippingFees += $productShipping;
+            $orderProducts[$products[$i]->id] = [
+                'id' => $products[$i]->id,
+                'name' => $products[$i]->name,
+                'price' => $products[$i]->price,
+                'final_price' => isset($products[$i]->final_price) ? $products[$i]->final_price : $products[$i]->price,
+                'quantity' => $products[$i]->bought_qty,
+                'shipping_fees' => $productShipping
+            ];
+        }
+        $cityShippings = CityShipping::all();
+        $key = array_search($request->city, array_column($cityShippings->toArray(), 'id'));
+        $totalShipping = ($isCustomShipping) ? $shippingFees + $cityShippings[$key]->shipping_fees : $shippingFees;
+        $orderData = array(
+            'first_name' => $request->first_name,
+            'last_name' => $request->last_name,
+            'phone' => $request->phone,
+            'email' => $request->email,
+            'address' => $request->address,
+            'city' => $request->city,
+            'products' => json_encode($orderProducts),
+            'status' => 'placed',
+            'quantity' => $total_qty,
+            'customer_message' => $request->message,
+            'payment_method' => $request->payment,
+            'shipping_fees' => $totalShipping,
+            'sub_total' => $total,
+            'total' => $total + $totalShipping,
+        );
+
+        $order = Order::create($orderData);
+
+        $cases = [];
+        $ids = [];
+        $params = [];
+        foreach ($products as $product) {
+            $cases[] = "WHEN {$product->id} then ?";
+            $params[] = $product->quantity - $product->bought_qty;
+            $ids[] = $product->id;
+        }
+        $ids = implode(',', $ids);
+        $cases = implode(' ', $cases);
+        $productsToUpdate = new Product;
+        $productsToUpdate->decreaseQuantity($ids, $params, $cases);
+        // Change Product Quantities
+        $orderStatusData = array(
+            'status' => 'placed',
+            'success' => true,
+            'ordering' => 0,
+            'order' => $order->id,
+            'message' => 'Order Placed Successfully',
+        );
+        OrderStatus::create($orderStatusData);
+        return redirect()->route('store');
     }
     function compare()
     {
@@ -226,8 +364,23 @@ class StoreController extends Controller
         }
         $children = new CategoriesLinker();
         $children = $children->getChildren(($parents));
-
-        return view('store.aproduct', ['product' => $product, 'images' => $images, 'typeSepcs' => $typeSepcs, 'parents' => $parentsData, 'children' => $children]);
+        $tags = ProductTags::where('product', $product->id)->select('tag')->get();
+        $productsByTags = ProductTags::whereIn('tag', $tags)->select('product')->distinct()->get();
+        $relatedProducts = new Product;
+        $relatedProducts = $relatedProducts->getRelatedProducts($product->category, $productsByTags);
+        for ($i = 0; $i < count($relatedProducts); $i++) {
+            if ($relatedProducts[$i]->enable_discount) {
+                $relatedProducts[$i]->final_price = $relatedProducts[$i]->price - ($relatedProducts[$i]->price * $relatedProducts[$i]->discount) / 100;
+            }
+        }
+        return view('store.aproduct', [
+            'product' => $product,
+            'relatedProducts' => $relatedProducts,
+            'images' => $images,
+            'typeSepcs' => $typeSepcs,
+            'parents' => $parentsData,
+            'children' => $children
+        ]);
     }
     function getCartProduct($id)
     {
