7f4aebdb02a40c6b296c17bdd904555c402daa1c
kamelkholy <kamelabdelkader17@gmail.com>
2021-02-08 13:26:41 +0200

    use mediumblob for images

--- database/migrations/2020_12_24_102657_create_brands_table.php
+++ database/migrations/2020_12_24_102657_create_brands_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
 
 class CreateBrandsTable extends Migration
 {
@@ -18,9 +19,9 @@ class CreateBrandsTable extends Migration
             $table->string('name');
             $table->string('name_ar');
             $table->integer('sortOrder')->default(0);
-            $table->binary('image');
             $table->timestamps();
         });
+        DB::statement("ALTER TABLE brands ADD image MEDIUMBLOB");
     }
 
     /**
--- database/migrations/2020_12_26_102127_create_categories_table.php
+++ database/migrations/2020_12_26_102127_create_categories_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
 
 class CreateCategoriesTable extends Migration
 {
@@ -19,11 +20,11 @@ class CreateCategoriesTable extends Migration
             $table->string('name_ar');
             $table->integer('sortOrder')->default(0);
             $table->integer('level')->default(0);
-            $table->binary('image')->nullable();
             $table->unsignedBigInteger('parent')->nullable();
             $table->boolean('show_image')->default(false);
             $table->timestamps();
         });
+        DB::statement("ALTER TABLE categories ADD image MEDIUMBLOB NULL");
     }
 
     /**
